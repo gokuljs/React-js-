62077596a805c4de7312a0b01e4dade123c0445b
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-03-17 16:00:09 +0530

    start building app

--- monster_rolodex/src/App.js
+++ monster_rolodex/src/App.js
@@ -1,26 +1,31 @@
-import React,{component} from 'react';
+// import React,{component} from 'react';
+import React, { Component } from 'react';
+
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends component{
+class App extends Component{
+  constructor(){
+    super();
+    // normally when you extend a componenet class and use to constructor super 
+    // wer are getting access to this.state and this.setstate
+    // component class is giving us acces to this two things one is where we store it other to used for updation 
+    this.state={
+        monster:[
+          {name:"frank"},
+          {name:"diablo"},
+          {name:"dracula"},
+          {name:"vampire"},
+          {name:"zombie"}
+        ]
+    };
+  }
   render(){
-    return(  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-          my name is gokul 
-        {/* Edit <code> src / App.js </code> and save to reload.{" "} */}
-      </p>{" "}
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React{" "}
-      </a>{" "}
-    </header>{" "}
-  </div>);
+    return (
+      <div className="App">
+
+  </div>
+    )
   }
 }
 
