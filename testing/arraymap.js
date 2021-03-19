6986459938a9336ef425ea3e9dc79dc9f1306236
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-03-19 15:13:46 +0530

    arraymap

---
+++ testing/arraymap.js
@@ -0,0 +1,6 @@
+// array methods 
+// map filters reducer find includes are some of the fucntions used in arrays 
+const myarr=[1,2,3,4,5,6]
+console.log(myarr)
+console.log(myarr.map(myarr=>myarr+1))
+console.log(myarr)
