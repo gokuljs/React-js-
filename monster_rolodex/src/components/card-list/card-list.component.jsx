4c728a60c1d698572139c522d7fbc6a577c35537
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-03-28 22:05:20 +0530

    card

--- monster_rolodex/src/components/card-list/card-list.component.jsx
+++ monster_rolodex/src/components/card-list/card-list.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react'; 
-import './card-list.styles.css'
+
+import {Card} from '../card/card.component';
+import './card-list.styles.css';
+
 // props is the parameter passsed inside cardlist from app.js file 
 // one of the most important property in props they have something called children
 //
 export const CardList=(props)=>(
     // console.log(props)
+
     // console.log(props.childreen)
     <div className="card-list">
+        
         {
-            props.monster.map(monster=>(<h1 key={monster.id}>{monster.name}</h1>))
+            props.monster.map(monster=>(
+
+                <Card key={monster.id} monster={monster}/>
+                // <h1 key={monster.id}>{monster.name}</h1>
+            ))
+        
+                
+        
         }
     </div>
 );
