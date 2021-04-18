11b3c127120c8c4a7ebd5857e827382e84d286ee
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-04-18 19:37:19 +0530

    testing

--- tic-tac-toe/tic-tac-toe/src/index.js
+++ tic-tac-toe/tic-tac-toe/src/index.js
@@ -1,3 +1,18 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
+
+class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Shopping List for {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
