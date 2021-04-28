c8017a359c788c8c28505315608dabc2f2de32e5
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-04-28 05:50:39 +0530

    asynchronous setstate

--- asynchronous-setstate/src/App.js
+++ asynchronous-setstate/src/App.js
@@ -1,25 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){// this allows you to use props inside constructor
+    super(props);
+    this.state={
+      meaningOfLife:47,
+    }
+  }
+
+  handleClick=()=>{
+  // one rule always to be followed in react 
+  // in your setstate calls when ever you use this.state or props then we dont pass it in form of objects like given below
+  // this.setState({meaningOfLife:this.state.meaningOfLife+1})
+  // so instead of passing functions with two parammeters like prevStates and prevprops
+    this.setState((prevState,prevProps)=>{
+      // this gaurantees the latest previouse state before update 
+      return {meaningOfLife:this.state.meaningOfLife+1}
+
+    },()=>console.log(this.state.meaningOfLife))
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+         
+          <button
+          onClick={this.handleClick}
+          >
+             Increment
+          </button>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
