35467e3a1d43ea4f55967678d08bd3eae32f6e86
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-04-28 14:21:43 +0530

    lifecycle component

--- life-cycle-methods/src/App.js
+++ life-cycle-methods/src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
+import Lifecycles from './lifecycles.component';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      showChild:true,
+      text:'',
+    };
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +19,30 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={()=>{
+            this.setState(state =>({
+              showChild:!state.showChild,
+            }))
+          }
+
+          }
         >
-          Learn React
-        </a>
+          Toggle lifecyle
+        </button>
+        <button
+          onClick={()=>
+          this.setState(state=>({
+            text:state.text+'_hello',
+          }))}
+        >
+          update text 
+        </button>
+        {this.state.showChild ? <Lifecycles test={this.state.text}/> :null}
       </header>
     </div>
   );
+  }
 }
 
 export default App;
