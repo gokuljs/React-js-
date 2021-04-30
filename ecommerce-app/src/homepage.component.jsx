dee6791da164150dc84ae2b4dac5105149b4ce01
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-04-30 17:18:30 +0530

    sass

--- ecommerce-app/src/homepage.component.jsx
+++ ecommerce-app/src/homepage.component.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import './homepage.styles.scss'
 // going to be functional component because dont need any life cycle methods 
 const HomePage=()=>(
     <div className="homepage">
     <div className="directory-menu">
-        <div className="menu=item">
+        <div className="menu-item">
             <div className="content">
                 <h1 className='title'>Hats</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
-        <div className="menu=item">
+        <div className="menu-item">
             <div className="content">
                 <h1 className='title'>Jackets</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
-        </div><div className="menu=item">
+        </div><div className="menu-item">
             <div className="content">
                 <h1 className='title'>Sneakers</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
-        <div className="menu=item">
+        <div className="menu-item">
             <div className="content">
                 <h1 className='title'>Womens</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
-        <div className="menu=item">
+        <div className="menu-item">
             <div className="content">
                 <h1 className='title'>Mens</h1>
                 <span className='subtitle'>SHOP NOW</span>
