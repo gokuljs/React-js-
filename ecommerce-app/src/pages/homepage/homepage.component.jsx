b72abae5d61eb0d37e3268389e793d1818edc917
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-05-01 11:58:54 +0530

    adding directory and menu-item sepreste component

--- ecommerce-app/src/pages/homepage/homepage.component.jsx
+++ ecommerce-app/src/pages/homepage/homepage.component.jsx
@@ -1,43 +1,10 @@
 import React from 'react';
+import Directory from '../../components/directory/directory.component.jsx'
 import './homepage.styles.scss'
 // going to be functional component because dont need any life cycle methods 
 const HomePage=()=>(
     <div className="homepage">
-    <div className="directory-menu">
-        <div className="menu-item">
-            <div className="content">
-                <h1 className='title'>Hats</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-        <div className="menu-item">
-            <div className="content">
-                <h1 className='title'>Jackets</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div><div className="menu-item">
-            <div className="content">
-                <h1 className='title'>Sneakers</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-        <div className="menu-item">
-            <div className="content">
-                <h1 className='title'>Womens</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-        <div className="menu-item">
-            <div className="content">
-                <h1 className='title'>Mens</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-        
-        
-
-    </div>
-
+    <Directory />
     </div>
 );
 export default HomePage;
