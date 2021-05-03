8440204453d4fed26dbfa3adeb83a390750a7b07
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-05-03 07:24:49 +0530

    menu-item styling

--- ecommerce-app/src/components/menu-item/menu-item.component.jsx
+++ ecommerce-app/src/components/menu-item/menu-item.component.jsx
@@ -8,6 +8,7 @@ const MenuItem=({title,imageUrl,size})=>(
     backgroundImage:`url(${imageUrl})`
 }}
 className={`${size} menu-item`}>
+<div className='background-image'/>
     <div className="content">
     <h1 className='title'>{title.toUpperCase()}</h1>
     <span className='subtitle'>SHOP NOW</span>
