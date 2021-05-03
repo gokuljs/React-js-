ce0b203cb44ada141fb76796d33b9bd12b7f3553
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-05-03 07:50:12 +0530

    updating menu item changes

--- ecommerce-app/src/components/menu-item/menu-item.component.jsx
+++ ecommerce-app/src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,12 @@ import './menu-item.styles.scss'
 // using functional component
 
 const MenuItem=({title,imageUrl,size})=>(
+<div 
+className={`${size} menu-item`}>
+
 <div style={{
     backgroundImage:`url(${imageUrl})`
-}}
-className={`${size} menu-item`}>
-<div className='background-image'/>
+}} className='background-image'/>
     <div className="content">
     <h1 className='title'>{title.toUpperCase()}</h1>
     <span className='subtitle'>SHOP NOW</span>
