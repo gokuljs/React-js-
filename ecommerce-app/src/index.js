936c608d9f80769ff997d605e59de94b840e0636
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-05-04 06:49:14 +0530

    react-router to your project

--- ecommerce-app/src/index.js
+++ ecommerce-app/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    {/* now we are passing all the react router functionality to browser router */}
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
